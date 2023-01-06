5e9f5c8075a4c6ac542c8aacc492d8f4bfb65fa8
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-01-06 04:57:01 -0300

    feat: POST /tweets

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const tweetsAvatares = [];
 const tweets = [];
 const usuarios = [];
 
@@ -27,11 +28,39 @@ server.post("/sign-up", (req, res) => {
 });
 
 server.post("/tweets", (req, res) => {
-    
+    const {username, tweet} = req.body;
+    const usuarioExiste = usuarios.find(iten => iten.username === username);
+
+    if(!username || !tweet){
+        res.status(400).send({error: "Todos os campos são obrigatórios"});
+        return;
+    };
+
+    if(!usuarioExiste){
+        res.sendStatus(401);
+        return;
+    };
+
+    const newTweets = {
+        username,
+        tweet
+    }
+
+    tweets.push(newTweets);
+
+    tweetsAvatares.push({
+        username,
+        avatar: usuarioExiste.avatar,
+        tweet
+    });
+
+    console.log(tweetsAvatares);
+
+    res.sendStatus(200);
 })
 
-// server.get("/tweets", (req, res) =>{
-//     res.send("blablabla blublu")
-// })
+server.get("/tweets", (req, res) =>{
+    res.send("blablabla blublu")
+})
 
 server.listen(5000, () => {"funfou"});
